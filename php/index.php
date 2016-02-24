42027816a840ef325985fc511b1698e5e3394082
jmendez1340 <jasonmendez795@gmail.com>
2016-02-24 15:34:54 -0600

    web115
    Web115

---
+++ php/index.php
@@ -0,0 +1,34 @@
+<! Doctype html> 
+<html>
+<head>
+    <title> PHP include</title>
+    <style>
+        body{
+            background-color: aquamarine;}
+        }
+        #container { width: 960px;
+        min-height:600px;
+        background: ivory;
+        color: aquamarine;
+        font-family:sans-serif;
+        font-size: 20px;}
+        
+        main {min-height: 960px;}
+        footer {min-height:50px;}
+    </style>
+    </head>
+
+<body>
+    <div id=" container">
+    <h1>Text should go here</h1>
+    
+    <main>
+    </main>
+    
+    <footer> <p style="text-align:center"> this is my footer</p>
+        </footer>
+   </div>
+    </body>
+
+
+</html>
